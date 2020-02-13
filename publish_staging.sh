71ee4211b82da478f4e858495f41ad6f1d52f126
karathan <karathan@noinfo.tdl>
2020-02-13 16:39:06 +0100

    fix staging port

--- publish_staging.sh
+++ publish_staging.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker container stop cexplore_staging || true && docker container rm cexplore_staging || true
-docker container run -d --name "cexplore_staging" --restart=always -it -p 10243:10243 karathan/cexplore:staging
+docker container run -d --name "cexplore_staging" --restart=always -it -p 10240:10243 karathan/cexplore:staging
