fdfff6efb0fa887895088eee55ebfa6430283fcd
laqieer <laqieer@126.com>
2020-03-06 14:37:30 +0800

    improve asm diff

--- pycat.py
+++ pycat.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
-import subprocess
-import sys
 import os
 import argparse
 import shutil
+import re
 
 parser = argparse.ArgumentParser(description='Simplified "compiler" frontend, cats any input')
 
@@ -15,6 +14,46 @@ args, remainder = parser.parse_known_args()
 if args.version:
     print("pycat frontend for cexplore")
     quit()
-    
+
 source = remainder[-1]
-shutil.copyfile(source,args.destination)
+
+with open(source, 'r') as f_src, open(args.destination, 'w') as f_dst:
+    for line in f_src:
+        line2 = line.strip()
+        if line2 and ':' not in line2 and not line2.startswith('@') and not line2.startswith('//'):
+            instruction = line2.split(maxsplit=1)
+            if len(instruction) > 1:
+                opcode, operand = instruction
+                if opcode.endswith('s'):
+                    opcode = opcode[:-1]
+                rx_ry = re.search(r'r\d{1,2}-r\d{1,2}', operand, re.I)
+                if rx_ry:
+                    rx_ry = rx_ry.group(0)
+                    pos = rx_ry.find('-')
+                    x = int(rx_ry[1:pos])
+                    y = int(rx_ry[pos + 2:])
+                    operands = operand.split(rx_ry, 1)
+                    operand = operands[0]
+                    for i in range(x, y + 1):
+                        operand += f'r{i}, '
+                    operand = operand[:-2] + operands[1]
+                dec_num = re.search(r'#\-?\d+', operand, re.I)
+                if dec_num:
+                    dec_num = dec_num.group(0)
+                    if dec_num[1] != '-':
+                        value = int(dec_num[1:])
+                        if value != 0:
+                            s = operand.split(dec_num, 1)
+                            operand = f'{s[0]}#0x{value:x}{s[1]}'
+                    else:
+                        value = int(dec_num[2:])
+                        if value != 0:
+                            s = operand.split(dec_num, 1)
+                            operand = f'{s[0]}#-0x{value:x}{s[1]}'
+                if opcode in ('add', 'sub', 'lsl', 'lsr', 'asr', 'ror', 'and', 'orr', 'eor'):
+                    operands = operand.split(',')
+                    if len(operands) == 2:
+                        operand = operands[0] + ', ' + operand
+                line2 = f'\t{opcode}\t{operand}'
+        f_dst.write(line2 + '\n')
+
