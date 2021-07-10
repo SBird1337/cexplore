059b9a4017a456497b5d88f1054c39ac5b4d563b
theo3 <arisstephenson2@gmail.com>
2021-07-09 17:22:56 -0700

    agbcp (c++) support for Mother 3

--- update-repos.sh
+++ update-repos.sh
@@ -4,3 +4,4 @@ cd /pretrepos/pokeemerald && git pull && make tools
 cd /pretrepos/pokefirered && git pull && make tools
 cd /pretrepos/pokeruby && git pull && make tools
 cd /pretrepos/fireemblem8u && git pull && make tools
+cd /pretrepos/mother3 && git pull && make tools
